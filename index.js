2fcc9ab8ae3054c5409e938c294dcf5aa6514a8e
Tevon Talbert <TevonTalbert@hotmail.com>
2023-08-20 00:09:59 -0500

    Adding homework

--- index.js
+++ index.js
@@ -1,1 +1,52 @@
 // Write your solution here!
+
+
+const cats = ["Milo", "Otis", "Garfield"]; 
+
+
+
+function destructivelyAppendCat(name) {
+ 
+  cats.push(name);
+
+
+}
+function destructivelyPrependCat(name) {
+  cats.unshift(name);
+}
+
+function destructivelyRemoveLastCat(name) {
+  cats.pop(name);
+}
+
+function destructivelyRemoveFirstCat(name) {
+  cats.shift(name);
+}
+
+function appendCat(name) {
+
+  var NewArray = cats.slice();
+  NewArray.push(name)
+  return NewArray;
+
+  
+
+ 
+}
+function prependCat(name) {
+  var NewArray = cats.slice();
+  NewArray.unshift(name)
+  return NewArray;
+
+}
+function removeLastCat(name) {
+  var NewArray = cats.slice();
+  NewArray.pop(name)
+  return NewArray;
+}
+function removeFirstCat(name) {
+  var NewArray = cats.slice();
+  NewArray.shift(name)
+  return NewArray;
+
+}
